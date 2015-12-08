c9d725a5f7239bad77692f8814fa84d56a5439a0
Wu Yuntao <wyt.brandon@gmail.com>
2015-12-08 17:00:04 +0800

    Add a skeleton model to validate the idea of generate bvh angles

--- BvhExporter/Data/MotionSet.cs
+++ BvhExporter/Data/MotionSet.cs
@@ -57,5 +57,180 @@ namespace BvhExporter.Data
                 return motion;
             }
         }
+
+        public static Motion Spine
+        {
+            get
+            {
+                var motion = new Motion();
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "SpineBase",
+                    HeadPos = new vec3(0, 0, 0),
+                    TailPos = new vec3(0, 0.3337337f, 0),
+                });
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "SpineMid",
+                    ParentName = "SpineBase",
+                    HeadPos = new vec3(0, 0.3337337f, 0),
+                    TailPos = new vec3(0, 0.581777f, 0),
+                });
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "SpineShoulder",
+                    ParentName = "SpineMid",
+                    HeadPos = new vec3(0, 0.581777f, 0),
+                    TailPos = new vec3(0, 0.66476926f, 0),
+                });
+
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "ShoulderLeft",
+                    ParentName = "SpineMid",
+                    HeadPos = new vec3(0, 0.581777f, 0),
+                    TailPos = new vec3(-0.1818914f, 0.581777f, 0),
+                });
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "ElbowLeft",
+                    ParentName = "ShoulderLeft",
+                    HeadPos = new vec3(-0.1818914f, 0.581777f, 0),
+                    TailPos = new vec3(-0.4112974f, 0.581777f, 0),
+                });
+
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "ShoulderRight",
+                    ParentName = "SpineMid",
+                    HeadPos = new vec3(0, 0.581777f, 0),
+                    TailPos = new vec3(0.1818914f, 0.581777f, 0),
+                });
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "ElbowRight",
+                    ParentName = "ShoulderRight",
+                    HeadPos = new vec3(0.1818914f, 0.581777f, 0),
+                    TailPos = new vec3(0.4112974f, 0.581777f, 0),
+                });
+
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "HipLeft",
+                    ParentName = "SpineBase",
+                    HeadPos = new vec3(0, 0, 0),
+                    TailPos = new vec3(-0.085884925f, 0, 0),
+                });
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "KneeLeft",
+                    ParentName = "HipLeft",
+                    HeadPos = new vec3(-0.085884925f, 0, 0),
+                    TailPos = new vec3(-0.085884925f, -0.4331289f, 0),
+                });
+
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "HipRight",
+                    ParentName = "SpineBase",
+                    HeadPos = new vec3(0, 0, 0),
+                    TailPos = new vec3(0.085884925f, 0, 0),
+                });
+                motion.Bones.Add(new Bone()
+                {
+                    Name = "KneeRight",
+                    ParentName = "HipRight",
+                    HeadPos = new vec3(0.085884925f, 0, 0),
+                    TailPos = new vec3(0.085884925f, -0.4331289f, 0),
+                });
+
+                // 1
+                var frame = new Frame() { Time = 1 };
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "SpineBase",
+                    HeadPos = new vec3(0.0113890832290053f, -0.44517907500267f, 2.30070400238037f),
+                    TailPos = new vec3(0.0218616798520088f, -0.145339205861092f, 2.44687032699585f),
+                });
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "SpineMid",
+                    ParentName = "SpineBase",
+                    HeadPos = new vec3(0.0218616798520088f, -0.145339205861092f, 2.44687032699585f),
+                    TailPos = new vec3(0.0291184559464455f, 0.0806832611560822f, 2.54878520965576f),
+                });
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "SpineShoulder",
+                    ParentName = "SpineMid",
+                    HeadPos = new vec3(0.0291184559464455f, 0.0806832611560822f, 2.54878520965576f),
+                    TailPos = new vec3(0.0315756686031818f, 0.156884253025055f, 2.5815737247467f),
+                });
+
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "ShoulderLeft",
+                    ParentName = "SpineMid",
+                    HeadPos = new vec3(0.0291184559464455f, 0.0806832611560822f, 2.54878520965576f),
+                    TailPos = new vec3(-0.153398245573044f, 0.0435472913086414f, 2.51644325256348f),
+                });
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "ElbowLeft",
+                    ParentName = "ShoulderLeft",
+                    HeadPos = new vec3(-0.153398245573044f, 0.0435472913086414f, 2.51644325256348f),
+                    TailPos = new vec3(-0.376345813274384f, 0.00909638404846191f, 2.51425409317017f),
+                });
+
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "ShoulderRight",
+                    ParentName = "SpineMid",
+                    HeadPos = new vec3(0.0291184559464455f, 0.0806832611560822f, 2.54878520965576f),
+                    TailPos = new vec3(0.193298190832138f, 0.0501831732690334f, 2.49732041358948f),
+                });
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "ElbowRight",
+                    ParentName = "ShoulderRight",
+                    HeadPos = new vec3(0.193298190832138f, 0.0501831732690334f, 2.49732041358948f),
+                    TailPos = new vec3(0.406055271625519f, 0.074210949242115f, 2.40489220619202f),
+                });
+
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "HipLeft",
+                    ParentName = "SpineBase",
+                    HeadPos = new vec3(0.0113890832290053f, -0.44517907500267f, 2.30070400238037f),
+                    TailPos = new vec3(-0.0626806020736694f, -0.432130515575409f, 2.2574610710144f),
+                });
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "KneeLeft",
+                    ParentName = "HipLeft",
+                    HeadPos = new vec3(-0.0626806020736694f, -0.432130515575409f, 2.2574610710144f),
+                    TailPos = new vec3(-0.108909033238888f, -0.869084060192108f, 2.151691198349f),
+                });
+
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "HipRight",
+                    ParentName = "SpineBase",
+                    HeadPos = new vec3(0.0113890832290053f, -0.44517907500267f, 2.30070400238037f),
+                    TailPos = new vec3(0.0867793336510658f, -0.441536575555801f, 2.26158285140991f),
+                });
+                frame.Bones.Add(new Bone()
+                {
+                    Name = "KneeRight",
+                    ParentName = "HipRight",
+                    HeadPos = new vec3(0.0867793336510658f, -0.441536575555801f, 2.26158285140991f),
+                    TailPos = new vec3(0.111000396311283f, -0.829505443572998f, 2.11823010444641f),
+                });
+
+                motion.Frames.Add(frame);
+
+                return motion;
+            }
+        }
     }
 }
--- BvhExporter/Program.cs
+++ BvhExporter/Program.cs
@@ -9,6 +9,14 @@ namespace BvhExporter
     {
         static void Main(string[] args)
         {
+            //SimpleTest();
+            SpineTest();
+
+            Console.ReadKey();
+        }
+
+        private static void SimpleTest()
+        {
             var motion = MotionSet.Simple1;
 
             foreach (var frame in motion.Frames)
@@ -60,8 +68,112 @@ namespace BvhExporter
                     Console.WriteLine("angles {0} vs {1}", angles / Math.PI * 180, boneFra.RotationAngles);
                 }
             }
+        }
 
-            Console.ReadKey();
+        private static void SpineTest()
+        {
+            var motion = MotionSet.Spine;
+
+            foreach (var frame in motion.Frames)
+            {
+                Console.WriteLine("==================================");
+                Console.WriteLine("Frame {0}", frame.Time);
+                foreach (var fBone in frame.Bones)
+                {
+                    var lQuat = GetAbsoluteRotation(fBone.Name, motion, frame, true);
+                    if (fBone.ParentName != null)
+                    {
+                        var pQuat = GetAbsoluteRotation(fBone.ParentName, motion, frame, false);
+                        //lQuat = pQuat.Inverse * lQuat;
+                        // TODO dot product order?
+                        lQuat = lQuat * pQuat.Inverse;
+                    }
+
+                    var lAngles = lQuat.EulerAngles / Math.PI * 180;
+
+                    Console.WriteLine("{0} [FINAL] q: ({1}), angles: ({2})", fBone.Name, lQuat, lAngles);
+                }
+            }
+        }
+
+        private static quat GetAbsoluteRotation(string boneName, Motion motion, Frame frame, bool printLog)
+        {
+            var dBone = motion.Bones.Find(b => b.Name == boneName);
+            var fBone = frame.Bones.Find(b => b.Name == boneName);
+
+            var dLen = (ConvertToZUp(dBone.HeadPos) - ConvertToZUp(dBone.TailPos)).Length;
+            var fLen = (ConvertToZUp(fBone.HeadPos) - ConvertToZUp(fBone.TailPos)).Length;
+
+            var dDir = ConvertToZUp(dBone.TailPos) - ConvertToZUp(dBone.HeadPos);
+            var fDir = ConvertToZUp(fBone.TailPos) - ConvertToZUp(fBone.HeadPos);
+
+            var q = FromToRotation(dDir, fDir);
+            var angles = q.EulerAngles / Math.PI * 180;
+
+            if (printLog)
+            {
+                Console.WriteLine("{0} len: {1} -> {2}", fBone.Name, dLen, fLen);
+                Console.WriteLine("{0} dir: ({1}) -> ({2})", fBone.Name, dDir, fDir);
+                Console.WriteLine("{0} q: ({1}), angles: ({2})", fBone.Name, q, angles);
+            }
+
+            return q;
+        }
+
+        private static vec3 ConvertToZUp(vec3 vec)
+        {
+            //return vec;
+            return new vec3(vec.x, -vec.z, vec.y);
+        }
+
+        private static quat FromToRotation(vec3 from, vec3 to)
+        {
+            from = from.Normalized;
+            to = to.Normalized;
+
+            var d = vec3.Dot(from, to);
+
+            if (d >= 1.0f)
+            {
+                // In the case where the two vectors are pointing in the same
+                // direction, we simply return the identity rotation.
+                return quat.Identity;
+            }
+            else if (d <= -1.0f)
+            {
+                // If the two vectors are pointing in opposite directions then we
+                // need to supply a quaternion corresponding to a rotation of
+                // PI-radians about an axis orthogonal to the fromDirection.
+                var axis = vec3.Cross(from, new vec3(1, 0, 0));
+                if (axis.LengthSqr < 1e-6)
+                {
+                    // Bad luck. The x-axis and fromDirection are linearly
+                    // dependent (colinear). We'll take the axis as the vector
+                    // orthogonal to both the y-axis and fromDirection instead.
+                    // The y-axis and fromDirection will clearly not be linearly
+                    // dependent.
+                    axis = vec3.Cross(from, new vec3(0, 1, 0));
+                }
+
+                // Note that we need to normalize the axis as the cross product of
+                // two unit vectors is not nececessarily a unit vector.
+                axis = axis.Normalized;
+                return quat.FromAxisAngle((float)Math.PI, axis);
+            }
+            else
+            {
+                // Scalar component.
+                var s = Math.Sqrt(from.LengthSqr * to.LengthSqr) + vec3.Dot(from, to);
+
+                // Vector component.
+                var v = vec3.Cross(from, to);
+
+                // Return the normalized quaternion rotation.
+                var rotation = new quat(v.x, v.y, v.z, (float)s);
+                rotation = rotation.Normalized;
+
+                return rotation;
+            }
         }
 
         static void PrintMat4(mat4 mat4)
